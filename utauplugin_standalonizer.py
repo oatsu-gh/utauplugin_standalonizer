d6f228e25fe4ab7fc891b5ff959466a0e9803634
oatsu <panchi.psp@gmail.com>
2020-11-03 01:31:38 +0900

    ファイルを作ってとりあえず動くところまで実装した。
    すっぴんプラグインがちゃんと動いた。UTAUエディタ上での実行と比較して、整合性チェックもした。

---
+++ utauplugin_standalonizer.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python3
+# coding: utf-8
+# Copyright (c) 2020 oatsu
+"""
+# 入力
+- ustファイルを読み取ってUstオブジェクトにする。
+- UstオブジェクトをUtauPluginオブジェクトに変換する。
+- UTAUプラグインへの入力用txtファイルに出力する。
+  - ノートを全選択したときのスクリプトと一致するようにする。
+# プラグイン実行
+- UTAUプラグインを起動してtxtファイルを渡す。
+- UTAUプラグインが終了したのを検知。
+- UTAUプラグインが上書きしたtxtファイルを取得。
+# 出力
+- UTAUプラグインが出力したファイルを読み取るってUtauPluginオブジェクトにする。
+- もとのUtauPluginオブジェクトのパラメーターと比較し、変更部分を上書きする。
+- 加工済みUtauPluginオブジェクトをもとのUstオブジェクトと比較し、Ustオブジェクトに変換する。
+- Ustオブジェクトをustファイルとして出力する。
+"""
+
+# from time import sleep
+import subprocess
+from copy import deepcopy
+from os.path import basename, splitext
+from pprint import pprint
+
+import utaupy as up
+from tqdm import tqdm
+
+PATH_TEMPORARY_PLUGINTXT = './temp_utauplugin_wrapper.txt'
+
+
+def run_external_utauplugin(path_utauplugin_exe, path_ust_in):
+    """
+    入出力パスを指定し、全体の処理を実行する。
+    """
+    # 処理したいUSTファイルを読み取る
+    original_ust = up.ust.load(path_ust_in)
+    # [#TRACKEND] を取り除く
+    original_ust_for_plugin = deepcopy(original_ust)
+    # UTAUプラグイン用のテキストファイルを出力
+    path_plugintxt = PATH_TEMPORARY_PLUGINTXT
+    original_ust_for_plugin.write(path_plugintxt.replace('.txt', '_in.txt'))
+    original_ust_for_plugin.write(path_plugintxt)
+    # UTAUプラグインを起動して処理させる
+    subprocess.run((path_utauplugin_exe, path_plugintxt), check=True)
+    # [#TRACKEND] を追加してから、UTAUプラグインが出力したファイルを取得する
+    with open(path_plugintxt, 'a') as f:
+        f.write('[#TRACKEND]\n')
+    configured_ust = up.ust.load(path_plugintxt)
+
+    # 変更点を元のUSTに反映
+    original_notes = original_ust.notes
+    for idx, note in enumerate(tqdm(configured_ust.notes)):
+        if note.tag == '[#INSERT]':
+            original_notes.insert(idx, note)
+        else:
+            # データ差分を更新
+            original_notes[idx].update(note)
+    # ustファイルを出力
+    path_ust_out=splitext(basename(path_ust_in))[0] + '_updated.ust'
+    original_ust.notes = original_notes
+    original_ust.write(path_ust_out)
+
+
+def main():
+    path_utauplugin_exe=input('path_utauplugin_exe: ').strip('"')
+    path_ust_in=input('path_ust_in: ').strip('"')
+    run_external_utauplugin(path_utauplugin_exe, path_ust_in)
+    input('できた')
+
+
+if __name__ == '__main__':
+    main()
